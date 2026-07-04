c09a00238eea5d018e9f35abdc38d37d5c46e5d2
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2026-07-04 22:40:17 +0300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,10 +5,16 @@ console.log("Logs from your program will appear here!");
 
 // TODO: Uncomment the code below to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
+  socket.on("close", (req, res) => {
+    res.writeHead(200, {'Host': '/index.html'})
+    socket.end(JSON.stringify({
+      data: 'HTTP/1.1 200 OK\r\n\r\n'
+    }));
   });
-  socket.write('HTTP/1.1 200 OK\r\n\r\n')
+  // socket.write('HTTP/1.1 200 OK\r\n\r\n')
+  socket.on('clientError', () => {
+    socket.enf('HTTP/1.1 404 Not Found\r\n\r\n')
+  })
 });
 
 server.listen(4221, "localhost");
