9cf8b92f1b994d3702056afa1a7115ea8f477a1a
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2026-07-04 23:44:09 +0300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
 
   socket.on("data", (data) => {
-    console.log(JSON.stringify(data.toString()))
+    console.log(JSON.parse(data.toString()))
     // const req = data.toString()
     socket.end()
   })
