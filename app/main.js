1e9dc68364d91764d831e691c272962ee284103b
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2026-07-04 22:53:18 +0300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -6,13 +6,14 @@ console.log("Logs from your program will appear here!");
 // TODO: Uncomment the code below to pass the first stage
 const server = net.createServer({ port: 4221 }, (socket) => {
   socket.on("close", () => {
-    socket.write('HTTP/1.1 200 OK\r\n\r\n')
     socket.end();
   });
   
-  server.on('error', (err) => {
+  socket.write('HTTP/1.1 200 OK\r\n\r\n')
+  
+  socket.on('error', (err) => {
     console.log('HTTP/1.1 404 Not Found\r\n\r\n')
-    server.end()
+    socket.end()
   })
 });
 
