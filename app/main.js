0f6f0d77e6f089afa644a958b7194dccc9b5a844
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2026-07-04 23:12:05 +0300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,16 +1,20 @@
 const net = require("net");
+const http = require('node:http')
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // TODO: Uncomment the code below to pass the first stage
 const server = net.createServer((socket) => {
-  
+  const req = http.get(options)
+  console.log(req)
   socket.on("close", () => {
     socket.end();
   });
-  socket.write(server)
+  
   socket.write('HTTP/1.1 200 OK\r\n\r\n')
+}).on('error', (err) => {
+  socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
 });
 
 
