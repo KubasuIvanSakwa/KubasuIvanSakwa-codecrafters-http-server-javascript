1a8ef08677346273a657d9ec2e84de0ab914faa7
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2026-07-04 23:39:37 +0300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,15 +5,18 @@ console.log("Logs from your program will appear here!");
 
 // TODO: Uncomment the code below to pass the first stage
 const server = net.createServer((socket) => {
+  
+  socket.on("data", (data) => {
+    console.log(data.toString())
+    socket.end()
+  })
+
   socket.on("close", () => {
     socket.end();
   });
 
-  socket.on("data", (data) => {
-    socket.write(data)
-  })
   
-  socket.write('HTTP/1.1 200 OK\r\n\r\n')
+  // socket.write('HTTP/1.1 200 OK\r\n\r\n')
 });
 
 
