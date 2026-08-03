3ed64b188f003ecbe1a16e85c2b307b625d42074
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2026-08-03 12:10:57 +0300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -35,8 +35,23 @@ const server = net.createServer((socket) => {
         } else if(url === '/user-agent' && MATCHUSERAGENT) {
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${MATCHUSERAGENT[1].length}\r\n\r\n${MATCHUSERAGENT[1]}`)
         } else if(url.includes('/files/')) {
-          const directory = process.argv[3]
-          console.log(directory)
+           const directory = process.argv[3];
+            const filename = url.split("/files/")[1];
+            const filePath = `../${directory}/${filename}`;
+            
+            if (fs.existsSync(filePath)) {
+                // Respond with the file content
+                const content = fs.readFileSync(filePath).toString();
+                const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${Buffer.byteLength(content)}\r\n\r\n${content}`;
+                socket.write(res);
+                socket.end();
+            } else {
+                // Respond with 404 Not Found
+                const res = 'HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n';
+                socket.write(res);
+            }
+            
+            socket.end();
         } else socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
       }
     }
